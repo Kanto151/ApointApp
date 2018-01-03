24245a6586eaa17d00456550c7fbdf269ce65c14
Robert <Robert@DESKTOP-FJ80DKU>
2018-01-03 16:45:09 +0800

    using another app as a base

---
+++ ApointOrgApp/ApointOrgApp/Main2.cs
@@ -0,0 +1,34 @@
+﻿//using System;
+//using CoreGraphics;
+//using AssetsLibrary;
+//using UIKit;
+//using Foundation;
+//using CoreImage;
+
+//namespace ApointOrgApp
+//{
+//    public class Application
+//    {
+//        UIView View;
+//        // This is the main entry point of the application.
+//        static void Main(string[] args)
+//        {
+//            // if you want to use a different Application Delegate class from "AppDelegate"
+//            // you can specify it here.
+//            UIApplication.Main(args, null, "AppDelegate");
+            
+//        }
+
+//        UIButton Button1;
+
+//        public void ViewDidLoad()
+//        {
+//            Button1 = UIButton.FromType(UIButtonType.RoundedRect);
+//            Button1.Frame = new CGRect(10, 80, 90, 40);
+//            Button1.SetTitle("Test", UIControlState.Normal);
+
+//            View.BackgroundColor = UIColor.White;
+
+//        }
+//    }
+//}
---
+++ ApointOrgApp/ApointOrgApp/pageUI.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using CoreGraphics;
+
+namespace ApointOrgApp
+{
+    class pageUI
+    {
+        UIButton Button1;
+
+        public void ViewDidLoad ()
+        {
+            Button1 = UIButton.FromType(UIButtonType.RoundedRect);
+            Button1.Frame = new CGRect(10, 80, 90, 40);
+            Button1.SetTitle("Test", UIControlState.Normal);
+
+        }
+    }
+}
